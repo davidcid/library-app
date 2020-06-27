12b3038d111c2eae82e99e1f27550f444786b596
David Cid <david.cid@gmail.com>
2020-06-27 11:21:37 +0200

    created login page

---
+++ src/components/Login.jsx
@@ -0,0 +1,60 @@
+import React, { useState } from "react";
+import { Form, Input, Button, Card } from "antd";
+import { Container } from "../styles/styled";
+import { LockOutlined, MailOutlined } from "@ant-design/icons";
+
+const Login = (props) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+  };
+
+  return (
+    <Container>
+      <Card title="Login" style={{ width: 300, textAlign: "center" }}>
+        <Form onSubmit={handleSubmit}>
+          <Input
+            prefix={<MailOutlined style={{ color: "rgba(0, 0, 0, 0.25)" }} />}
+            placeholder="Email"
+            style={{ marginBottom: "10px" }}
+            onChange={(e) => {
+              setEmail(e.target.value);
+            }}
+            autoComplete="off"
+          />
+          <Input
+            prefix={<LockOutlined style={{ color: "rgba(0, 0, 0, 0.25)" }} />}
+            placeholder="Password"
+            style={{ marginBottom: "10px" }}
+            onChange={(e) => {
+              setPassword(e.target.value);
+            }}
+            autoComplete="off"
+          />
+          <Button
+            type="primary"
+            htmlType="submit"
+            className="login-form-button"
+            style={{ width: "100%", marginBottom: "10px" }}
+          >
+            Login
+          </Button>
+          Or
+          <Button
+            type="primary"
+            style={{ width: "100%", marginTop: "10px", marginBottom: "10px" }}
+            onClick={() => {
+              props.history.push("/register");
+            }}
+          >
+            Register
+          </Button>
+        </Form>
+      </Card>
+    </Container>
+  );
+};
+
+export default Login;
